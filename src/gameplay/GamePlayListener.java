068066d0a9c410b419c9812c988426a8496fd1b3
Ahmed Sayed <ahmedsayedgfx@gmail.com>
2021-01-04 18:18:58 +0200

    Refactor Pause Screen to Dialog

--- src/gameplay/GamePlayListener.java
+++ src/gameplay/GamePlayListener.java
@@ -3,6 +3,7 @@ package gameplay;
 import gameassets.Constants;
 import gameassets.Globals;
 
+import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -18,10 +19,14 @@ public class GamePlayListener implements KeyListener {
     public void keyPressed(KeyEvent evt) {
         if (evt.getKeyCode() == KeyEvent.VK_PAUSE) {
             try {
-                Globals.timer.counter.setText("PAUSE");
                 Globals.pauseGame();
                 Constants.MAIN_MUSIC.stop();
-                new maingui.PauseScreen().setVisible(true);
+                Object[] options = {"Resume", "Main Menu"};
+                int n = JOptionPane.showOptionDialog(Globals.gamePlay, "Do you want to resume the game?", "",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+                if (n == JOptionPane.YES_OPTION) Globals.resumeGame();
+                else if (n == JOptionPane.NO_OPTION) Globals.endGame();
+                else Globals.endGame();
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
